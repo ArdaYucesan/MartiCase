c1a957aaba00f38ac8775438d48593dadc5fac11
arda-yucesan <54549911+ArdaYucesan@users.noreply.github.com>
2025-04-11 14:40:06 +0300

    refactor : added safeCall utility function for api calls

---
+++ app/src/main/java/com/ardayucesan/marticase/map_screen/data/network/utils/safeCall.kt
@@ -0,0 +1,41 @@
+package com.ardayucesan.marticase.map_screen.data.network.utils
+
+import com.ardayucesan.marticase.map_screen.domain.utils.NetworkError
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.GlobalScope.coroutineContext
+import kotlinx.coroutines.ensureActive
+import kotlinx.serialization.SerializationException
+import kotlinx.coroutines.DelicateCoroutinesApi
+import com.ardayucesan.marticase.map_screen.domain.utils.Result
+
+@OptIn(DelicateCoroutinesApi::class)
+suspend inline fun <reified T> safeCall(
+    execute: () -> HttpResponse
+): Result<T, NetworkError> {
+    val response = try {
+        execute()
+    } catch (e: UnresolvedAddressException) {
+        return Result.Error(
+            NetworkError.UnresolvedAddress(
+                e.message ?: "Unresolved address exception"
+            )
+        )
+    } catch (e: SerializationException) {
+        return Result.Error(
+            NetworkError.SerializationError(
+                e.message ?: "Serialization error exception"
+            )
+        )
+    } catch (e: Exception) {
+        //TODO: bunu tekrardan incele detaylarına bak
+        println("genel exception içindeyim")
+        e.printStackTrace()
+        coroutineContext.ensureActive()
+
+        return Result.Error(NetworkError.Unknown(e.message ?: "Unknown error exception"))
+    }
+
+    return responseToResult(response)
+
+}
